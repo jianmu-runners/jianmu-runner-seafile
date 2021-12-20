b30e914996ea460ec921ae95896ab5fec0f5f29a
hx <1014231151@qq.com>
2021-12-20 17:00:07 +0800

    feat: 上传覆盖功能实现

--- python/seafile_uploads.py
+++ python/seafile_uploads.py
@@ -10,6 +10,27 @@ repo_id = os.getenv("JIANMU_REPO_ID")
 dir_path = os.getenv("JIANMU_DIR_PATH")
 upload_file_path = os.getenv("JIANMU_UPLOAD_FILE_PATH")
 
+# 对输入参数进行一些前置操作
+upload_file_name = upload_file_path.split("/")[-1]
+if not (base_url.endswith("/")):
+    base_url = base_url + "/"
+
+# 获取用户的token
+acquire_token_data = {"username": username,
+                      "password": password}
+try:
+    response = requests.post(base_url + 'api2/auth-token/', data=acquire_token_data)
+except Exception as e:
+    print("请求失败，请检查参数后重试，具体错误信息：", e)
+    sys.exit(1)
+
+token = response.text[10:50]
+
+# 构建headers
+headers = {
+    'Authorization': 'Token ' + token
+}
+
 # 处理返回结果
 def handleResult(response):
     # 生成结果文件
@@ -33,78 +54,66 @@ def handleResult(response):
     print(json.dumps(resultJson, sort_keys=True, indent=2) + "")
     # resultJson ="{\"remote_file_url\" : \""+ remote_file_url +"\"}"
 
-# 对输入参数进行一些前置操作
-upload_file_name = upload_file_path.split("/")[-1]
-if not (base_url.endswith("/")):
-    base_url = base_url + "/"
+def update():
+    """以下是执行更新操作"""
+    # 获取更新链接
+    try:
+        response = requests.get(base_url + 'api2/repos/' + repo_id + '/update-link/',
+                                headers=headers)
+    except Exception as e:
+        print("请求失败，请检查参数后重试，具体错误信息：", e)
+        sys.exit(1)
 
-# 获取用户的token
-acquire_token_data = {"username": username,
-                      "password": password}
-try:
-    response = requests.post(base_url + 'api2/auth-token/', data=acquire_token_data)
-except Exception as e:
-    print("请求失败，请检查参数后重试，具体错误信息：", e)
-    sys.exit(1)
+    update_link = response.text.replace("\"", "")
 
-token = response.text[10:50]
+    # 执行更新
+    update_dir_path = dir_path + upload_file_name if dir_path.endswith("/") else dir_path + "/" +upload_file_name
 
-# 构建headers
-headers = {
-    'Authorization': 'Token ' + token
-}
-
-# 上传之前，尝试更新文件，更新成功即成功覆盖源文件。更新失败执行上传操作
-# 以下是执行更新操作
-
-# 获取更新链接
-try:
-    response = requests.get(base_url + 'api2/repos/' + repo_id + '/update-link/',
-                            headers=headers)
-except Exception as e:
-    print("请求失败，请检查参数后重试，具体错误信息：", e)
-    sys.exit(1)
-
-update_link = response.text.replace("\"", "")
-
-# 执行更新
-update_dir_path = dir_path + upload_file_name if dir_path.endswith("/") else dir_path + "/" +upload_file_name
-
-files = {
-    'file': (upload_file_name, open(upload_file_path, 'rb')),
-    'target_file': (None, update_dir_path),
-}
-
-try:
-    response = requests.post(update_link, headers=headers, files=files)
-except Exception as e:
-    print("请求失败，请检查参数后重试，具体错误信息：", e)
-    sys.exit(1)
-
-# 更新成功
-if not response.status_code == 441:
-    handleResult(response)
-    exit(0)
-
-# 更新失败，进行上传操作
-# 获得上传链接
-try:
-    response = requests.get(base_url + 'api2/repos/' + repo_id + '/upload-link/',
-                            headers=headers)
-except Exception as e:
-    print("请求失败，请检查参数后重试，具体错误信息：", e)
-    sys.exit(1)
-
-upload_link = response.text.replace("\"", "")
-
-# 执行上传
-try:
     files = {
         'file': (upload_file_name, open(upload_file_path, 'rb')),
-        'parent_dir': (None, dir_path)
+        'target_file': (None, update_dir_path),
     }
-    response = requests.post(upload_link, headers=headers, files=files)
-    handleResult(response)
-except Exception as e:
-    print("请求失败，请检查参数后重试。具体错误信息：", e)
-    sys.exit(1)
+
+    try:
+        response = requests.post(update_link, headers=headers, files=files)
+    except Exception as e:
+        print("请求失败，请检查参数后重试，具体错误信息：", e)
+        sys.exit(1)
+
+    # 更新成功
+    if not response.status_code == 441:
+        handleResult(response)
+        exit(0)
+
+def upload():
+    """以下是执行上传操作"""
+    # 获得上传链接
+    try:
+        response = requests.get(base_url + 'api2/repos/' + repo_id + '/upload-link/',
+                                headers=headers)
+    except Exception as e:
+        print("请求失败，请检查参数后重试，具体错误信息：", e)
+        sys.exit(1)
+
+    upload_link = response.text.replace("\"", "")
+
+    # 执行上传
+    try:
+        files = {
+            'file': (upload_file_name, open(upload_file_path, 'rb')),
+            'parent_dir': (None, dir_path)
+        }
+        response = requests.post(upload_link, headers=headers, files=files)
+        handleResult(response)
+    except Exception as e:
+        print("请求失败，请检查参数后重试。具体错误信息：", e)
+        sys.exit(1)
+
+def batchUpload():
+    """以下是执行批量上传操作"""
+    pass
+
+# 上传之前，尝试更新文件，更新成功即成功覆盖源文件。更新失败执行上传操作
+update()
+# 更新失败，进行上传操作
+upload()
